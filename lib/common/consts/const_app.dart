05e82f66fca0214d3d4bafaaecf1d42151d9a2e2
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-25 01:24:54 +0100

    FEAT: adicionado imgs e const das images ao asset

---
+++ lib/common/consts/const_app.dart
@@ -0,0 +1,7 @@
+class ConstApp {
+  static String workPath = "lib/common/assets/img/Airplane.png";
+  static String allPath = "lib/common/assets/img/All.png";
+  static String bookPath = "lib/common/assets/img/Book.png";
+  static String headphonesPath = "lib/common/assets/img/Headphones.png";
+  static String airplanePath = "lib/common/assets/img/Airplane.png";
+}
