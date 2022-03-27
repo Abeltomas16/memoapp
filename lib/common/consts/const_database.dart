08567ea8ed5d528bfbf949fb1bc47964eb62394c
Abeltomas16 <abeljosetoms115@gmail.com>
2022-03-28 00:20:42 +0100

    FEAT: resolvendo problemas

--- lib/common/consts/const_database.dart
+++ lib/common/consts/const_database.dart
@@ -1,6 +1,9 @@
 class ConstsDatabase {
+  // ignore: non_constant_identifier_names
   static String DATABASE_NAME = 'dbTodo';
+  // ignore: non_constant_identifier_names
   static String TABLE_NAME = 'tarefas';
+  // ignore: non_constant_identifier_names
   static String CREATE_TAREFA_TABLE_SRCIPT =
       "CREATE TABLE tarefas(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,descricao TEXT,categoria TEXT, dataInicar TEXT, terminado INTEGER,dataTerminado TEXT)";
 }
